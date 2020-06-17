43adc6fb005bd9ebb1613fc554b80f36dfb4e794
Ernesto Canquiz <fdsoil123@gmail.com>
2020-06-17 10:58:42 -0400

    Lesson 3 - Separation Of Data Between The Model And The View.

---
+++ Part4CreateAndUseComponents/Lesson03SeparationOfDataBetweenTheModelAndTheViewPart1/app.js
@@ -0,0 +1,87 @@
+Vue.component('app-icon', {
+    template:'<span :class="cssClasses" aria-hidden="true"></span>',
+    props: ['img'],
+    computed: {
+        cssClasses: function () {
+            return 'glyphicon glyphicon-' + this.img;
+        }
+    }
+});
+
+Vue.component('app-task', {
+    template: "#task-template",
+    props: ['tasks','task', 'index'],
+    methods: {
+        toggleStatus: function() {
+            this.task.pending = !this.task.pending;
+        },
+        edit: function () {
+            this.tasks.forEach(function (task) {
+                task.editing = false;
+            });
+            this.draft = this.task.description;
+            this.task.editing = true;
+        },
+        update: function () {
+            this.task.description = this.draft;
+            this.task.editing = false;
+        },
+        discard: function () {
+            this.task.editing = false;
+        },
+        remove: function () {
+            this.tasks.splice(this.index, 1);
+        },
+
+
+    }        
+});                            
+
+var vm = new Vue({
+    el: "#app",
+    methods: {
+        createTask: function () {
+            this.tasks.push({
+                description: this.new_task,
+                pending: true,
+                editing: false
+            });
+            this.new_task = '';
+        },
+        deleteCompleted: function () {
+            this.tasks = this.tasks.filter(function (task) {
+               return task.pending;
+            });
+        }
+    },
+    created: function () {
+        //this.tasks.forEach( function (task) {
+        //    this.$set(task, 'editing', false);
+        //}.bind(this));
+        //this.tasks.forEach( (task) => {
+        //    this.$set(task, 'editing', false);
+        //});
+        this.tasks.forEach( task => {
+            this.$set(task, 'editing', false);
+        });
+    },
+    data: {
+        draft: '',
+        new_task: '',
+        tasks: [
+            {
+                description: 'Aprender Vue.js',
+                pending: true
+            },
+            {
+                description: 'Subcribirse a Style.net',
+                pending: true
+            },
+            {
+                description: 'Grabar lección de Vue',
+                pending: false
+            }
+        ]
+    }
+});
+
